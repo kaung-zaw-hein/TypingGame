ef36de1cf39eccef112d78a4932eeebd030f4681
Kaung Zaw Hein <kaungzawhein1963@gmail.com>
2024-02-21 22:33:19 +0630

    text setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import { useEffect, useState } from "react";
+import { generate } from "random-words";
 
 function App() {
 
   const [isGameOver, setIsGameOVer ] = useState(false);
-  const [text, setText]  = useState("");
+  const [textList, setTextList] = useState([]);
   const [input, setInput]  = useState("");
+  const [currWordIndex, setCurrWordIndex] = useState(0)
+  const [currCharIndex, setCurrCharIndex] = useState(-1)
+  const [currChar, setCurrChar] = useState("")
+  const [correct, setCorrect] = useState(0)
   const [ score, setScore ] = useState(0);
   const [ timer, setTimer ] = useState(0);
   const [ startTime, setStartTime ] = useState(0);
-  
-  let randomTexts = [
-    "Technology has revolutionized the way we live, work, and interact with one another. From the invention of the internet to the development of smartphones, technological advancements have transformed every aspect of modern life. Today, we rely on technology for communication, transportation, healthcare, entertainment, and much more. As technology continues to evolve at a rapid pace, it presents both opportunities and challenges for society.",
-    "Climate change is one of the most pressing issues facing our planet today. The burning of fossil fuels, deforestation, and industrial activities have led to a rise in greenhouse gas emissions, resulting in global warming and unpredictable weather patterns. The consequences of climate change are far-reaching, affecting ecosystems, biodiversity, and human populations worldwide. Urgent action is needed to mitigate the impacts of climate change and transition to a sustainable future."
-  ]
+  const [ status, setStatus ] = useState("Start");
 
+  
   const start = () => {
-    let radomItem = randomTexts[Math.floor((Math.random() * randomTexts.length -1))];
-    setText(radomItem);
-    setTimer(6); 
+    setTextList("");
+    setTextList(generate(100));
+    setTimer(600); 
     setScore(0); 
     setIsGameOVer(false); 
     setStartTime(Date.now());
+    setStatus("Reset")
   }
   
   const handleChange = (e) => {
@@ -29,16 +32,21 @@ function App() {
 
   const endGame = () => {
     setIsGameOVer(true);
+    setStatus("Play Again!")
 
     const endTime = Date.now();
     const elapsedTime = (endTime - startTime) / 1000;
-    console.log(elapsedTime);
+    
   }
 
   useEffect(() => {
-    window.addEventListener("keydown",(event)=>{
-      console.log(event);
-  })
+    setTextList(generate(100));
+  }, []);
+
+  useEffect(() => {
+  //   window.addEventListener("keydown",(event)=>{
+  //     console.log(event);
+  // })
     let interval;
     if(timer > 0 && !isGameOver){
       interval = setInterval(() => {
@@ -54,18 +62,40 @@ function App() {
  
 
   return (
-    <div className="flex flex-col bg-[#323437] text-neutral-300 items-center justify-center h-screen px-10">
+    <div className="flex flex-col bg-[#323437] text-neutral-300 items-center justify-center h-screen px-20 ">
       <h1 className="mb-8 text-3xl font-bold">Speed Typing Game</h1>
       {!isGameOver ? (
         <>
-          <p className="mb-4 text-2xl leading-10 text-neutral-500 opacity-55">{text}</p>
-          <textarea
-            className="w-64 h-24 p-2 mb-4 border border-gray-300 rounded-md"
-            placeholder="Type here..."
+          <div className="relative flex flex-wrap w-full mb-4 text-2xl leading-1">{
+            textList.map((word, i) => (
+              <div className=" opacity-20 text-neutral-200"  key={i}>
+                {word.split("").map((char, i) => (
+                  <span key={i} >{char}</span>
+                ))}
+                {i !== textList.length - 1 && <span>&nbsp;</span>} 
+              </div>
+            ))
+          }
+          {/* <div className="absolute inset-0 z-20 flex flex-wrap ">
+          {
+            textList.map((word, i) => (
+              <div className={false? " " :" "} key={i}>
+                {word.split("").map((char, i) => (
+                  <span key={i} className={false ? "text-white " : " text-red-700 "}>{char}</span>
+                ))}
+                {i !== textList.length - 1 && <span>&nbsp;</span>} 
+              </div>
+            ))
+          }
+          </div>  */}
+          <input
+            className="absolute inset-0 bg-transparent opacity-0 "
             value={input}
+            onKeyDown={(e) => { console.log(e)}}
             onChange={handleChange}
             autoFocus
-          ></textarea>
+          />
+          </div>
           <p>Time:{timer}s</p>
           <p>Score: {score}</p>
         </>
@@ -79,7 +109,7 @@ function App() {
         className="px-4 py-2 mt-4 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
         onClick={start}
       >
-        {isGameOver ? 'Play Again' : 'Start Game'}
+        {status}
       </button>
     </div>
   );
