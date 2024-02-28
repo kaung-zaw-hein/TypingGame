e7828b3bd8ed5130dab99b0e82913e9e2ea6f278
Kaung Zaw Hein <kaungzawhein1963@gmail.com>
2024-02-28 22:28:10 +0630

    fix bugs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { generate } from "random-words";
 
 function App() {
   const [isGameOver, setIsGameOver] = useState(false);
   const [textList, setTextList] = useState([]);
+  const [typedList, setTypedList] = useState([]);
   const [score, setScore] = useState(0);
   const [timer, setTimer] = useState(0);
   const [startTime, setStartTime] = useState(0);
@@ -12,9 +13,14 @@ function App() {
   const [typedText, setTypedText] = useState("");
   const [accuracy, setAccuracy] = useState(100);
   const [wordsPerMinute, setWordsPerMinute] = useState(0);
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    start();
+  }, []);
 
   const start = () => {
-    setTextList(generate(100));
+    setTextList(generate(50));
     setTimer(60);
     setScore(0);
     setIsGameOver(false);
@@ -22,43 +28,52 @@ function App() {
     setStatus("Reset");
     setCurrWordIndex(0);
     setTypedText("");
+    setTypedList([]);
+    if(inputRef?.current){
+      inputRef.current.focus(); 
+      inputRef.current.value = "";
+    }
   };
 
   const handleChange = (e) => {
     if (!isGameOver) {
+      if (currWordIndex >= textList.length - 1) {
+        endGame();
+      }
       // Handle backspace
       if (e.key === "Backspace") {
         if (typedText.length > 0) {
           setTypedText((prevTypedText) => prevTypedText.slice(0, -1));
+        } else if (typedList.length > 0) {
+          setCurrWordIndex((prevIndex) => prevIndex - 1);
+          setTypedText(typedList[typedList.length - 1]);
+          setTypedList((prevList) => prevList.slice(0, -1));
         }
         return;
       }
-  
-      // Handle whitespace
-      if (e.key === " ") {
-        setTypedText((prevTypedText) => prevTypedText + " ");
-        return;
-      }
-  
+
       // Get the expected character
       const expectedChar = textList[currWordIndex][typedText.length];
-  
       // Check if the typed character matches the expected character
+      if (e.key === " ") {
+        setCurrWordIndex((prevIndex) => prevIndex + 1);
+        setTypedList((prevList) => prevList.concat(typedText));
+        setTypedText("");
+        if (typedText.length === textList[currWordIndex].length) {
+          setScore((prevScore) => prevScore + 1);
+        } else {
+          setAccuracy((prevAccuracy) => prevAccuracy - 1);
+        }
+        return;
+      }
       if (e.key === expectedChar) {
         setTypedText((prevTypedText) => prevTypedText + e.key);
-        if (e.key === " " || typedText.length === textList[currWordIndex].length - 1) {
-          setCurrWordIndex((prevIndex) => prevIndex + 1);
-          setTypedText("");
-          setScore((prevScore) => prevScore + 1);
-        }
       } else {
-        // If mistyped, update accuracy and set typed text with red color
-        setAccuracy((prevAccuracy) => prevAccuracy - 1);
         setTypedText((prevTypedText) => prevTypedText + e.key);
+        setAccuracy((prevAccuracy) => prevAccuracy - 1);
       }
     }
   };
-  
 
   const endGame = () => {
     setIsGameOver(true);
@@ -73,10 +88,6 @@ function App() {
   };
 
   useEffect(() => {
-    setTextList(generate(100));
-  }, []);
-
-  useEffect(() => {
     let interval;
     if (timer > 0 && !isGameOver) {
       interval = setInterval(() => {
@@ -87,32 +98,48 @@ function App() {
       clearInterval(interval);
     }
     return () => clearInterval(interval);
-  }, [timer]);
+  }, [timer, isGameOver]);
 
   return (
-    <div className="flex flex-col bg-[#323437] text-neutral-300 items-center justify-center h-screen px-20 ">
+    <div className="flex flex-col bg-[#323437] text-neutral-200 items-start justify-center h-screen px-20 ">
       <h1 className="mb-8 text-3xl font-bold">Speed Typing Game</h1>
       {!isGameOver ? (
         <>
-          <div className="relative flex flex-wrap w-full mb-4 text-2xl leading-1">
+         <p>Time: {timer}s</p>
+          <div className="relative flex flex-wrap w-full mb-4 text-2xl leading-1 text-neutral-600">
             {textList.map((word, i) => (
-              <span
-                key={i}
-                className={i === currWordIndex ? "text-yellow-500" : undefined}
-              >
-                {word}&nbsp;
-              </span>
+              <div>
+                <span
+                  key={i}
+                  className={`${currWordIndex > i && (word === typedList[i] ? "text-green-300" : 'border-b-2 border-solid border-red-500')} ${i === currWordIndex ? "text-neutral-300" : undefined}`}
+                >
+                  {word.split('').map((char, index) => (
+                    <span
+                      key={index}
+                      className={index === typedText.length && i === currWordIndex ? "border-b-2 border-solid border-yellow-500" : undefined}
+                      style={{ color: (i === currWordIndex && (index < typedText.length ? (char !== typedText[index] ? 'red' : 'green') : (index === typedText.length && i === currWordIndex ? 'yellow' : ''))) ? (index < typedText.length ? (char !== typedText[index] ? 'red' : 'green') : (index === typedText.length && i === currWordIndex ? 'yellow' : '')) : undefined }}
+                    >
+                      {char}
+                    </span>
+                  ))}
+                </span>
+                <span>
+                  &nbsp;
+                </span>
+              </div>
             ))}
             <input
               className="absolute inset-0 bg-transparent opacity-0 "
               onKeyDown={handleChange}
+              ref={inputRef}
               autoFocus
             />
           </div>
-          <p>Time: {timer}s</p>
+          <div className="flex justify-end w-full space-x-12">
           <p>Score: {score}</p>
           <p>Accuracy: {accuracy}%</p>
           <p>Words Per Minute (WPM): {wordsPerMinute}</p>
+          </div>
         </>
       ) : (
         <>
